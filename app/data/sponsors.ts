8680fa2095d60e9d34d9a4f49eecca688f484706
YukiNo <tahanacibe@gmail.com>
2025-12-11 14:58:50 +0100

    add more sponsors

--- app/data/sponsors.ts
+++ app/data/sponsors.ts
@@ -49,14 +49,59 @@ const PARTNERS = [
 ];
 
 const SPONSORS: Sponsor[] = [
+  new Sponsor(
+    '/logo/sponsors/megafete.svg',
+    'gold',
+    '',
+    'Mega Fête',
+    'Salle pour les mariages, fiançailles, soutenances, fêtes de circoncision, séminaires et anniversaires',
+    'https://www.facebook.com/megafete25/',
+    ''
+  ),
+  new Sponsor(
+    '/logo/sponsors/media_smart.png',
+    'silver',
+    'https://mediasmart.dz/',
+    'Media Smart',
+    'MediaSmart stands out as a leading agency, entirely dedicated to the creation and realization of tailor-made projects, perfectly adapted to the specific requirements of your sector of activity',
+    'https://www.facebook.com/people/Media-Smart-Agence-de-communication-D%C3%A9veloppement-Event/100069324909602/?modal=admin_todo_tour#',
+    'https://www.linkedin.com/company/media-smart-communication-web-marketing-events/posts/?feedView=all'
+  ),
+  new Sponsor(
+    '/logo/sponsors/create_dz.png',
+    'silver',
+    'https://www.createdz.com/',
+    'Create Dz',
+    'CREATE is a communication agency specializing in website creation, graphic design and web development in constantine Algeria',
+    "https://www.facebook.com/p/Create-Dz-100076671481585/",
+    'https://dz.linkedin.com/in/create-dz-b54085281'
+  ),
+  new Sponsor(
+    '/logo/sponsors/socode.svg',
+    'silver',
+    'https://socode.tech/en',
+    'SoCode',
+    'La première école à Constantine spécialisée dans nouvelles technologie et les métier du numérique.',
+    "https://www.facebook.com/socode.school.dz/",
+    'https://www.linkedin.com/company/socode-school/'
+  ),
+  new Sponsor(
+    '/logo/sponsors/nexo_pizza.png',
+    'bronze',
+    'https://nexopizza.com/',
+    'Nexo Pizza',
+    'La première école à Constantine spécialisée dans nouvelles technologie et les métier du numérique.',
+    "https://www.facebook.com/nexopizza/",
+    ''
+  ),
   // new Sponsor(
-  //   '/logo/sponsors/megafete.svg',
-  //   'gold',
-  //   '',
-  //   'Mega Fête',
-  //   '',
-  //   'https://www.facebook.com/aiesec.constantine.75/',
-  //   'https://www.linkedin.com/company/aiesec/'
+  //   '/logo/sponsors/nexo_pizza.png',
+  //   'bronze',
+  //   'https://nexopizza.com/',
+  //   'Miss Chocolata',
+  //   'La première école à Constantine spécialisée dans nouvelles technologie et les métier du numérique.',
+  //   "https://www.facebook.com/nexopizza/",
+  //   ''
   // ),
 ]
 
